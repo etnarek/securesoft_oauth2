d8c3464616e9f982adbf5be8df092797e10dd4d8
Romain Fontaine <romain-fontaine@outlook.com>
2016-12-20 21:59:21 +0100

    Defferent serializer for detail and list vieuw for List and show urls for api

--- server/todo/serializers.py
+++ server/todo/serializers.py
@@ -5,11 +5,27 @@ from rest_framework import serializers
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Todo
-        fields = ('id', 'todo',)
+        fields = ('id', 'url', 'todo', 'todo_list')
 
 
 class ListSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = List
+        fields = ('id', 'url', 'name')
+
+    def create(self, validated_data):
+        name = validated_data.pop("name")
+        user = self.context["request"].user
+        l = List.objects.create(name=name, user=user)
+
+class ListSerializerDetail(serializers.HyperlinkedModelSerializer):
     todos = TodoSerializer(many=True, read_only=True)
     class Meta:
         model = List
-        fields = ('id', 'name', 'todos')
+        fields = ('id', 'url', 'name', 'todos')
+
+    def create(self, validated_data):
+        name = validated_data.pop("name")
+        user = self.context["request"].user
+        l = List.objects.create(name=name, user=user)
+        return l
--- server/todo/views.py
+++ server/todo/views.py
@@ -2,21 +2,28 @@ from django.shortcuts import render
 
 from .models import List, Todo
 from rest_framework import viewsets
-from .serializers import ListSerializer, TodoSerializer
+from .serializers import ListSerializer, TodoSerializer, ListSerializerDetail
 from server.permissions import IsOwner
 
 
 class ListViewSet(viewsets.ModelViewSet):
     queryset = List.objects.all()
     serializer_class = ListSerializer
+    permission_classes = (IsOwner, )
 
     def get_queryset(self):
         user = self.request.user
         return List.objects.filter(user=user)
 
+    def retrieve(self, *args, **kwargs):
+        print("hello")
+        self.serializer_class = ListSerializerDetail
+        return super(ListViewSet, self).retrieve(*args, **kwargs)
+
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    permission_classes = (IsOwner, )
 
     def get_queryset(self):
         user = self.request.user
