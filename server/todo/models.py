92af396a002a92681f6bfd89ce2752f1bae171cc
Romain Fontaine <romain-fontaine@outlook.com>
2016-12-20 19:09:15 +0100

    Base model for todo lists

--- server/todo/models.py
+++ server/todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class List(models.Model):
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.name
+
+
+class Todo(models.Model):
+    todo = models.CharField(max_length=255)
+    todo_list = models.ForeignKey(List, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.todo
